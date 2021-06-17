ff2613df479c47faff46ba1c1bce674ff38a68fe
OnurBoynuegri <onurboynuegri@hotmail.com>
2021-06-18 02:49:49 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic Constraint T nin alabileceğil data türlerini sınırlandıma işlemidir.
+    //T:class = referas tip olabilir demek
+    //IEntitiy = IEntity olabilir ya da IEntity implemente eden bir nesne olabilir demek.
+    //new() = newlene bilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        //GetAll sınıfını çağırırken filtreleme için expression kullanılır.
+        //mesela business katmanından productManager classından çağırılırken Linq kullanarak sadece product'ların fitayının ya da ıd sinin getirilmesini sağlamak için kullanılır.
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            _products = new List<Product> {
+                new Product{ProductId=1,CategoryId=1, ProductName="Bardak",UnitPrice=15,UnitsInStock=15 },
+                new Product{ProductId=2,CategoryId=2, ProductName="Kamera",UnitPrice=500,UnitsInStock=3 },
+                new Product{ProductId=3,CategoryId=2, ProductName="Telefon",UnitPrice=1500,UnitsInStock=2 },
+                new Product{ProductId=4,CategoryId=2, ProductName="Klavye",UnitPrice=150,UnitsInStock=65 },
+                new Product{ProductId=5,CategoryId=2, ProductName="Fare",UnitPrice=85,UnitsInStock=1 },
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+
+            //bu şekilde listedeki ürünü silemeyiz çünkü referans value olduğu için product'ın referansı farklı geliyor ve
+            //kayıtlı ürünlerin referansı ile aynı olmuyor
+            //_products.Remove(product); 
+            //bu tür durumda ürünleri döngü içinde productId leri karşılaştırarak aramak yerine Linq'ları kullanırız.
+            //LINQ-Language Intagrated Query
+            //genelde ID olan aramalarda SingleOrDefault methodu kullanılır.
+
+            Product productToDelete = _products.SingleOrDefault(p=>p.ProductId== product.ProductId);
+            _products.Remove(productToDelete);
+
+
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            //where içiresindeki koşula uyan bütün elemanları yeni bir liste yapar ve bu listeyi gönderir.
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
